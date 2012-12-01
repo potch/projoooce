7ba12c949f39a4a9119394283e96873afae48219
Matt Claypotch <thepotch@gmail.com>
2012-12-01 12:40:25 -0800

    aww yeah

--- static/js/app.js
+++ static/js/app.js
@@ -2,13 +2,14 @@ function loadImg(src) {
     var d = $.Deferred();
     var i = new Image();
 
-    i.src = src;
     i.onload = function() {
         d.resolve(i);
     };
     i.onerror = function() {
         d.reject();
     };
+
+    i.src = src;
 
     return d;
 }
@@ -18,32 +19,43 @@ function Mortar(numCols, parent) {
 
     var cols = [];
 
-    for (var i=0; i<numCols.length; i++) {
+    parent = $(parent);
+
+    for (var i=0; i<numCols; i++) {
         cols.push({
-            pos: 0,
+            y: 0,
             el: $("<div class='col'>")
         });
-        $(parent).push(cols[i].el);
+        parent.append(cols[i].el);
     }
 
     self.append = function(img) {
 
-        var goodCol = 0;
+        img = $(img);
 
-        for (var i = numCols-1; i >= 0; i--) {
-            if (cols[i].pos < cols[goodCol].pos) {
+        var goodCol = numCols-1;
+
+        for (var i = numCols - 1; i >= 0; i--) {
+            if (cols[i].y < cols[goodCol].y) {
                 goodCol = i;
             }
         }
 
         var col = cols[goodCol];
+        img.addClass('pinimg');
+        img.attr('width', 192);
         col.el.append(img);
-        col.pos += img.height;
+        col.y += img.height();
+
+        setTimeout(function() {
+            console.log('barf');
+            img.addClass('show');
+        }, 0);
 
     };
 }
 
-var mortar = new Mortar(2, document.body);
+var mortar = new Mortar(4, document.body);
 
 $.get('/pins/kkoberger90', function(r) {
     r.forEach(function(i) {
