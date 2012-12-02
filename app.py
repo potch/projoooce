10ae5ab634a541ca1e05161951e3348f3c4aaefb
Chris Van <cvan@mozilla.com>
2012-12-02 14:29:26 -0800

    faces

--- app.py
+++ app.py
@@ -52,19 +52,17 @@ def pins(user=None, shuffle=False):
 
 
 def home():
-    matches = {'mwindebank': 'mwindebank-1347468925_600.jpg',
-               'juliettesierra': 'juliettesierra_1344218153_600.jpg',
-               'callmemonalisa': 'callmemonalisa_1344993669_600.jpg',
-               #'cassyjmorris': 'cassyjmorris-1346242050_600.jpg',
-               #'hollyrob92': 'hollyrob92-1353702856_600.jpg',
-               #'tollilolly': 'tollilolly-52_600.jpg',
-               #'melaniexeinalem': 'melaniexeinalem-87_600.jpg'
-               }
-    random_matches = {}
-    for k, v in matches.iteritems():
-        if len(random_matches) < 3:
-            random_matches[k] = v
-    return render_template('index.html', matches=random_matches)
+    matches = [
+        ('mwindebank', 'mwindebank-1347468925_600.jpg'),
+        ('samanthakiser7', 'samanthakiser7-1353020567_600.jpg'),
+        ('callmemonalisa', 'callmemonalisa_1344993669_600.jpg'),
+        #'cassyjmorris', 'cassyjmorris-1346242050_600.jpg',
+        #'juliettesierra', 'juliettesierra_1344218153_600.jpg',
+        #'hollyrob92', 'hollyrob92-1353702856_600.jpg',
+        #'tollilolly', 'tollilolly-52_600.jpg',
+        #'melaniexeinalem', 'melaniexeinalem-87_600.jpg'
+    ]
+    return render_template('index.html', matches=matches)
 
 
 @app.route('/')
